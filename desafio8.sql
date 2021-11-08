346655d72dc0747f81a335d0562502b792608430
Erick Santos <erickosantos.dev@gmail.com>
2021-11-08 16:20:33 -0300

    desafio 8 fix

--- desafio8.sql
+++ desafio8.sql
@@ -6,10 +6,10 @@ FOR EACH ROW
 BEGIN
     DELETE
         FROM followed
-        WHERE OLD.fol_use_id = fol_use_id;
+        WHERE OLD.use_id = fol_use_id;
     DELETE
         FROM history_played
-        WHERE OLD.his_use_id = his_use_id;
+        WHERE OLD.use_id = his_use_id;
 END $$
 
 DELIMITER ;
